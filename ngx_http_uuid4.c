93bdc7d3ac956564e6f61c943022e543ab6cfa0d
Kristian Takvam <kristian@brilliant.org>
2019-05-26 07:14:46 -0700

    Source random bytes straight from /dev/urandom instead of using Mersenne Twister.

--- ngx_http_uuid4.c
+++ ngx_http_uuid4.c
@@ -3,7 +3,6 @@
 #include <ngx_config.h>
 #include <ngx_core.h>
 #include <ngx_http.h>
-#include "mt19937/mt64.h"
 
 
 static char *ngx_http_uuid4(ngx_conf_t *cf, ngx_command_t *cmd, void *conf);
@@ -53,53 +52,42 @@ ngx_module_t  ngx_http_uuid4_module = {
 };
 
 
-static int mt_initialized = 0;
-
-
-static ngx_int_t
-initialize_mt(ngx_http_request_t *r)
-{
-    static const size_t SEED_LENGTH = 312;
-    unsigned long long  seed[SEED_LENGTH];
-    size_t              n;
-    FILE               *f;
-
-    f = fopen("/dev/urandom", "r");
-    if (f == NULL) {
-        ngx_log_error(NGX_LOG_ERR, r->connection->log, 0,
-                      "failed to open /dev/urandom");
-        return NGX_ERROR;
-    }
-    n = fread(seed, sizeof(unsigned long long), SEED_LENGTH, f);
-    if (n < SEED_LENGTH) {
-        ngx_log_error(NGX_LOG_ERR, r->connection->log, 0,
-                      "failed to read /dev/urandom");
-        fclose(f);
-        return NGX_ERROR;
-    }
-    fclose(f);
-    init_by_array64(seed, SEED_LENGTH);
-
-    return NGX_OK;
-}
-
-
 static ngx_int_t
 ngx_http_uuid4_variable(ngx_http_request_t *r, ngx_http_variable_value_t *v,
     uintptr_t data)
 {
     static const size_t UUID_STR_LENGTH = 36;
+    static FILE        *urandom_fh;
+    static uint64_t     buf[2];
 
-    if (!mt_initialized) {
-        if (initialize_mt(r) != NGX_OK)
+    if (!urandom_fh) {
+        urandom_fh = fopen("/dev/urandom", "r");
+        if (urandom_fh == NULL) {
+            ngx_log_error(NGX_LOG_ERR, r->connection->log, 0,
+                        "failed to open /dev/urandom");
             return NGX_ERROR;
-        mt_initialized = 1;
+        }
     }
 
-    uint64_t upper = (uint64_t)genrand64_int64();
-    uint64_t lower = (uint64_t)genrand64_int64();
+    size_t n = fread(buf, sizeof(uint64_t), 2, urandom_fh);
+    if (n < 2) {
+        ngx_log_error(NGX_LOG_ERR, r->connection->log, 0,
+                      "failed to read /dev/urandom");
+        fclose(urandom_fh);
+        urandom_fh = NULL;
+        return NGX_ERROR;
+    }
+
+    uint64_t upper = buf[0];
+    uint64_t lower = buf[1];
+
+    // Adjust certain bits according to RFC 4122 section 4.4 as follows:
+    // set the four most significant bits of the 7th byte to 0100'B, so the high nibble is "4"
     upper &= ~((1ULL << 12) | (1ULL << 13) | (1ULL << 15));
     upper |= (1ULL << 14);
+
+    // set the two most significant bits of the 9th byte to 10'B, so the high nibble will be one
+    // of "8", "9", "A", or "B"
     lower &= ~(1ULL << 62);
     lower |= (1ULL << 63);
 
